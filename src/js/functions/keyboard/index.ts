50e1103d8c0230a3c381ae0bbd80e6fccd40c8c6
alexandr.kochurov <alexandr.kochurov@nutnet.ru>
2021-01-16 20:59:42 +0400

    c4zh4b KeyboardEvent

---
+++ src/js/functions/keyboard/index.ts
@@ -0,0 +1,26 @@
+'use strict'
+
+import keyPressed from './keyPressed'
+import {
+  isEscapeKey,
+  isEnterKey,
+  isBackspaceKey,
+  isDeleteKey,
+  isSpaceKey,
+  isPasteEvent,
+  isCopyEvent,
+  isSelectAllEvent,
+} from './checkKeys'
+
+export {
+  keyPressed,
+
+  isEscapeKey,
+  isEnterKey,
+  isBackspaceKey,
+  isDeleteKey,
+  isSpaceKey,
+  isPasteEvent,
+  isCopyEvent,
+  isSelectAllEvent,
+}
--- src/js/main.ts
+++ src/js/main.ts
@@ -6,6 +6,16 @@ import { hide, show, toggle } from './functions/show-hide-toggle'
 import { App } from './functions/app'
 import { getSearchParams } from './functions/url'
 import { loadScript, simulateMouseClick, smoothScroll } from './functions/html'
+import { keyPressed,
+  isEscapeKey,
+  isEnterKey,
+  isBackspaceKey,
+  isDeleteKey,
+  isSpaceKey,
+  isPasteEvent,
+  isCopyEvent,
+  isSelectAllEvent,
+} from './functions/keyboard'
 
 
 // *** скрипты для отдельных страниц
@@ -24,6 +34,16 @@ declare global {
     getSearchParams: Function,
     smoothScroll: Function,
     getTransitionEndEventName: Function,
+    keyPressed: Function,
+
+    isEscapeKey: Function,
+    isEnterKey: Function,
+    isBackspaceKey: Function,
+    isDeleteKey: Function,
+    isSpaceKey: Function,
+    isPasteEvent: Function,
+    isCopyEvent: Function,
+    isSelectAllEvent: Function,
 
     opera: any,
     chrome: any,
@@ -45,6 +65,17 @@ window.addEventListener('load', () => {
   window.loadScript = loadScript || {} // добавляет в html тег <script> с ссылкой на скрипт, который нужно загрузить
   window.simulateMouseClick = simulateMouseClick || {} // симулировать клик мыши по html элементу
   window.smoothScroll = smoothScroll || {} // плавный скролл к цели
+
+  window.keyPressed = keyPressed || {} // обработка нажатия клавиш
+
+  window.isEscapeKey = isEscapeKey || {} // нажата клавиша Esc
+  window.isEnterKey = isEnterKey || {} // нажата клавиша Enter
+  window.isBackspaceKey = isBackspaceKey || {} // нажата клавиша Backspace
+  window.isDeleteKey = isDeleteKey || {} // нажата клавиша Delete
+  window.isSpaceKey = isSpaceKey || {} // нажата клавиша Space
+  window.isPasteEvent = isPasteEvent || {} // нажаты клавиши Ctrl + V
+  window.isCopyEvent = isCopyEvent || {} // нажаты клавиши Ctrl + C
+  window.isSelectAllEvent = isSelectAllEvent || {} // нажаты клавиши Ctrl + A
 
   // сделать параметры доступными из любого места программы
   window.App = App || {} // основные параметры
