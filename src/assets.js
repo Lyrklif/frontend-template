7eb6d397cb70e14ef6e2d87754f5fc385f6e68e5
alexandr.kochurov <alexandr.kochurov@nutnet.ru>
2021-01-16 14:33:03 +0400

    init

---
+++ src/assets.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-undef */
+const requireAll = r => r.keys().forEach(r)
+
+// include svg-sprite
+requireAll(require.context('./ico/', true, /\.svg$/))
+// include pug-templates
+requireAll(require.context('./pug/', false, /\.pug$/))
